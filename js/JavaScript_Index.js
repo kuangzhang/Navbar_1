a5501bb2b5dbaa96af50c2ce61b4e9af918ce3ef
kuangzhang <kuangzhang@gmail.com>
2018-01-11 22:49:43 -0500

    Add files via upload

---
+++ js/JavaScript_Index.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    var active1 = false;
+    var active2 = false;
+    var active3 = false;
+    var active4 = false;
+
+    $('.parent2').on('mousedown touchstart', function () {
+
+        if (!active1) $(this).find('.test1').css({ 'background-color': 'gray', 'transform': 'none'  });
+        else $(this).find('.test1').css({ 'background-color': 'dimGray', 'transform': 'translate(0px,125px)'});
+
+        if (!active2) $(this).find('.test2').css({ 'background-color': 'gray', 'transform': 'none'  });
+        else $(this).find('.test2').css({ 'background-color': 'darkGray', 'transform': 'translate(-60px,105px)' });
+
+        if (!active3) $(this).find('.test3').css({ 'background-color': 'gray', 'transform': 'none' });
+        else $(this).find('.test3').css({ 'background-color': 'silver', 'transform': 'translate(-105px,60px)' });
+
+        if (!active4) $(this).find('.test4').css({ 'background-color': 'gray', 'transform': 'none' });
+        else $(this).find('.test4').css({ 'background-color': 'silver', 'transform': 'translate(-125px,0px)' });
+        active1 = !active1;
+        active2 = !active2;
+        active3 = !active3;
+        active4 = !active4;
+
+    });
+});
